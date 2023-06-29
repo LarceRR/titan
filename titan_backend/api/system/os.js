0691bddc1b2464ef9673acad7abcccd6cc5d737f
LarceRR <larcerrtc@gmail.com>
2023-06-29 17:00:56 +0300

    Rename variable in api/system/os.js
    cpu > os

--- titan_backend/api/system/os.js
+++ titan_backend/api/system/os.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const cpu = express.Router()
+const os = express.Router()
 const si = require('systeminformation')
 
-cpu.get('/os', function(req,res) {
+os.get('/os', function(req,res) {
     si.osInfo()
         .then(data => {
             res.send(data)
         })
         .catch(error => {
             res.send({
-                code: 'cpu_10',
+                code: 'os_10',
                 code_state: 'error',
                 code_message: error
             })
         })
 })
 
-module.exports = cpu
+module.exports = os
